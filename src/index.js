e5c0d26be895d54701d868791de5f93c4c401605
Ankit Tanwar <tankitofficial01@gmail.com>
2023-12-19 10:19:22 +0530

    comit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { createContext, useContext, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+
+
+
+let rootReducer=(preState , action)=>{
+
+let newState = preState
+
+switch(action.type){
+  case 'A':
+   break;
+   
+
+    default:
+      return newState;
+}
+
+return newState;
+
+}
+
+const store = createStore(rootReducer,{name:'OKLABS'})
+
+
+console.log( store.getState())
+
+let data = store.getState().name;
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   
+   
+
+    <App props={data}/>
+   
+   
+
+   
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
